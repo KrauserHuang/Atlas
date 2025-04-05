dccb7a94c6c3fe3c4ba512caab4fba8b85c14ab0
Krauser Huang <crescentdence@gmail.com>
2025-04-05 11:42:19 +0800

    Initial Commit

---
+++ Atlas/AtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasApp.swift
+//  Atlas
+//
+//  Created by Tai Chin Huang on 2025/4/5.
+//
+
+import SwiftUI
+
+@main
+struct AtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
