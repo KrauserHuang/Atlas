38caba5f781329a7d11f726ba91031732954003f
Andrew Huang <vivatv.it0001@gmail.com>
2025-05-27 17:18:46 +0800

    Create CLLocationCoordinate2D extension
    1. Create a dummy data taipei101

---
+++ Atlas/Extensions/CLLocationCoordinate2D.swift
@@ -0,0 +1,14 @@
+//
+//  CLLocationCoordinate2D.swift
+//  Atlas
+//
+//  Created by IT-MAC-02 on 2025/5/26.
+//
+
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static var taipei101: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 25.033649, longitude: 121.564824)
+    }
+}
