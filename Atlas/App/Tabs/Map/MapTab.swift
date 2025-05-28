c11d890f27702d084506460c7aaeb07335ce2b7f
Andrew Huang <vivatv.it0001@gmail.com>
2025-05-28 17:47:52 +0800

    Remove commented code add look around view
    1. Update Marker using MKMapItem
    2. Add system map scope button(MapUserLocationButton/MapPitchToggle/MapCompass)
    3. Show MKLookAroundScene when select location

--- Atlas/App/Tabs/Map/MapTab.swift
+++ Atlas/App/Tabs/Map/MapTab.swift
@@ -37,70 +37,55 @@ struct MapTab: View {
     @State private var selectedMapItem: MKMapItem?
     @State private var searchResults: [SearchResult] = []
     @State private var selectedLocation: SearchResult?
+    // 街景場景
     @State private var scene: MKLookAroundScene?
     // 地圖範圍命名空間，用於控制地圖相關的 UI 元件
     @Namespace var mapScope
     
     var body: some View {
         ZStack {
-//            Map(position: $position, selection: $selectedMapItem, scope: mapScope) {
-////                ForEach(mapItems, id: \.self) { mapItem in
-////                    Marker(item: mapItem)
-////                }
-////                Marker("Taipei 101", coordinate: .taipei101)
-//                Annotation("Taipei 101", coordinate: .taipei101, anchor: .bottom) {
-//                    ZStack {
-//                        RoundedRectangle(cornerRadius: 10)
-//                            .fill(.background)
-//                        Image(systemName: "house")
-//                            .padding(5)
-//                    }
-//                }
-//                UserAnnotation()
-//            }
-//            .mapControlVisibility(.hidden)
-//            .mapStyle(.standard(elevation: .realistic))
-//            .onChange(of: locationManager.region) {
-//                withAnimation {
-//                    position = .region(locationManager.region)
-//                }
-//            }
-//            .onMapCameraChange { context in
-//                visibleRegion = context.region
-//            }
-//            .overlay(alignment: .topTrailing) {
-//                VStack {
-//                    Spacer()
-//                    MapUserLocationButton(scope: mapScope)
-//                    MapPitchToggle(scope: mapScope)
-//                    MapCompass(scope: mapScope)
-//                        .mapControlVisibility(.visible)
-//                    Spacer()
-//                }
-//                .padding(.trailing, 10)
-//                .buttonBorderShape(.roundedRectangle)
-//            }
-//            .mapScope(mapScope)
-//            .sheet(isPresented: $isSheetPresented) {
-//                SearchSheetView(query: $query, isSearching: $isSearching, searchResults: $searchResults)
-//            }
-            
-            Map(position: $position, selection: $selectedLocation) {
-                ForEach(searchResults) { result in
-                    Marker(result.name, coordinate: result.location)
+            Map(position: $position, selection: $selectedLocation, scope: mapScope) {
+                ForEach(searchResults, id: \.self) { result in
+                    // 直接使用MKMapItem創建標記取得更多資訊
+                    Marker(item: result.mapItem)
                         .tag(result)
                 }
+                UserAnnotation()
             }
+            .mapControlVisibility(.hidden)              // 隱藏預設地圖控制選項
+            .mapStyle(.standard(elevation: .realistic))
+            .onChange(of: locationManager.region) {
+                // 當locationManager區域變動，更新相機
+                withAnimation {
+                    position = .region(locationManager.region)
+                }
+            }
+            .onMapCameraChange { context in
+                // 相機變化監聽，更新可見區域，用於追蹤使用者當前查看的地圖範圍
+                visibleRegion = context.region
+            }
+            .overlay(alignment: .topTrailing) {
+                VStack {
+                    Spacer()
+                    MapUserLocationButton(scope: mapScope)  // 定位使用者位置按鈕
+                    MapPitchToggle(scope: mapScope)         // 2D/3D切換
+                    MapCompass(scope: mapScope)             // 指南針
+                        .mapControlVisibility(.visible)
+                    Spacer()
+                }
+                .padding(.trailing, 10)
+                .buttonBorderShape(.roundedRectangle)
+            }
+            .mapScope(mapScope)
             .overlay(alignment: .bottom) {
                 if selectedLocation != nil {
-                    LookAroundPreview(scene: $scene, allowsNavigation: false, badgePosition: .bottomTrailing)
+                    LookAroundPreview(scene: $scene, allowsNavigation: true, badgePosition: .bottomTrailing)
                         .frame(height: 150)
                         .clipShape(RoundedRectangle(cornerRadius: 12))
                         .safeAreaPadding(.bottom, 40)
                         .padding(.horizontal, 20)
                 }
             }
-            .ignoresSafeArea()
             .onChange(of: selectedLocation) {
                 if let selectedLocation {
                     Task {
@@ -122,6 +107,9 @@ struct MapTab: View {
         }
     }
     
+    /// 取得指定座標 Look Around 街景場景
+    /// - Parameter coordinate: 目標位置座標
+    /// - Returns: 街景場景實例
     private func fetchScene(for coordinate: CLLocationCoordinate2D) async throws -> MKLookAroundScene? {
         let lookAroundScene = MKLookAroundSceneRequest(coordinate: coordinate)
         return try await lookAroundScene.scene
