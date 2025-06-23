f30f1ac32dc1801b47530c6679cb5f5fcc9aa0ce
Krauser Huang <crescentdence@gmail.com>
2025-06-23 09:00:13 +0800

    Update Authentication flow

--- Atlas/App/Auth/AuthenticatedView.swift
+++ Atlas/App/Auth/AuthenticatedView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 extension AuthenticatedView where Unauthenticated == EmptyView {
     init(@ViewBuilder content: @escaping () -> Content) {
@@ -20,6 +21,7 @@ struct AuthenticatedView<Content, Unauthenticated>: View where Content: View, Un
 //    @EnvironmentObject var viewModel: AuthenticationViewModel
     @State private var presentingLoginScreen: Bool = false
     @State private var presentingMainScreen: Bool = false
+    @State var selectedTab: AppTab = .map
     
     var unauthenticated: Unauthenticated?
     @ViewBuilder var content: () -> Content
@@ -29,13 +31,75 @@ struct AuthenticatedView<Content, Unauthenticated>: View where Content: View, Un
         self.content = content
     }
     
+//    var body: some View {
+//        switch viewModel.authenticationState {
+//        case .unauthenticated, .authenticating:
+//            AuthenticationView()
+//                .environmentObject(viewModel)
+//        case .authenticated:
+//            Text("You are logged in as \(viewModel.displayName)")
+//        }
+//    }
+    
     var body: some View {
         switch viewModel.authenticationState {
         case .unauthenticated, .authenticating:
-            AuthenticationView()
-                .environmentObject(viewModel)
+            VStack {
+                if let unauthenticated = unauthenticated {
+                    unauthenticated
+                }
+                else {
+                    Text("You're not logged in.")
+                }
+                Button("Tap here to log in") {
+                    viewModel.reset()
+                    presentingLoginScreen.toggle()
+                }
+            }
+            .sheet(isPresented: $presentingLoginScreen) {
+                AuthenticationView()
+                    .environmentObject(viewModel)
+            }
         case .authenticated:
-            Text("You are logged in as \(viewModel.displayName)")
+            VStack {
+                content()
+                Text("You're logged in as \(viewModel.displayName).")
+                Button("Tap here to view your profile") {
+                    presentingMainScreen.toggle()
+                }
+            }
+            .onReceive(NotificationCenter.default.publisher(for: ASAuthorizationAppleIDProvider.credentialRevokedNotification)) { event in
+                viewModel.signOut()
+                if let userInfo = event.userInfo, let info = userInfo["info"] {
+                    print(info)
+                }
+            }
+            .sheet(isPresented: $presentingMainScreen) {
+                NavigationView {
+                    mainTabView
+                        .environmentObject(viewModel)
+                }
+            }
+        }
+    }
+    
+    private var mainTabView: some View {
+        TabView(selection: $selectedTab) {
+            ForEach(AppTab.allCases) { tab in
+                tab.makeContentView(selectedTab: $selectedTab)
+                    .tabItem {
+                        tab.label
+                    }
+                    .tag(tab)
+            }
+        }
+        .onAppear {
+            // Make tab bar non-transparent
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor.systemBackground
+            UITabBar.appearance().standardAppearance = appearance
+            UITabBar.appearance().scrollEdgeAppearance = appearance
         }
     }
 }
--- Atlas/AtlasApp.swift
+++ Atlas/AtlasApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
@@ -18,37 +19,62 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct AtlasApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-//    @StateObject var authViewModel = AuthenticationViewModel()
+    @StateObject var authViewModel = AuthenticationViewModel()
     @State var selectedTab: AppTab = .map
     
     var body: some Scene {
         WindowGroup {
-            TabView(selection: $selectedTab) {
-                ForEach(AppTab.allCases) { tab in
-                    tab.makeContentView(selectedTab: $selectedTab)
-                        .tabItem {
-                            tab.label
-                        }
-                        .tag(tab)
+//            Group {
+//                switch authViewModel.authenticationState {
+//                case .authenticated:
+//                    mainTabView
+//                case .authenticating:
+//                    ProgressView()
+//                default:
+//                    AuthenticationView()
+//                        .environmentObject(authViewModel)
+//                }
+//            }
+            NavigationView {
+                AuthenticatedView {
+                    VStack {
+                        Image(systemName: "number.circle.fill")
+                            .resizable()
+                            .frame(width: 100 , height: 100)
+                            .foregroundColor(Color(.systemPink))
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .clipped()
+                            .padding(4)
+                            .overlay(Circle().stroke(Color.black, lineWidth: 2))
+                        Text("Welcome to Favourites!")
+                            .font(.title)
+                        Text("You need to be logged in to use this app.")
+                        mainTabView
+                        Spacer()
+                    }
                 }
             }
-//            TabView {
-//                MapView()
-//                ListView()
-//                ProfileView()
-//                AuthenticatedView {
-//                    Text("HI")
-//                }
-//                .environmentObject(authViewM odel)
-//            }
-            .onAppear {
-                // Make tab bar non-transparent
-                let appearance = UITabBarAppearance()
-                appearance.configureWithOpaqueBackground()
-                appearance.backgroundColor = UIColor.systemBackground
-                UITabBar.appearance().standardAppearance = appearance
-                UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+    
+    private var mainTabView: some View {
+        TabView(selection: $selectedTab) {
+            ForEach(AppTab.allCases) { tab in
+                tab.makeContentView(selectedTab: $selectedTab)
+                    .tabItem {
+                        tab.label
+                    }
+                    .tag(tab)
             }
+        }
+        .onAppear {
+            // Make tab bar non-transparent
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor.systemBackground
+            UITabBar.appearance().standardAppearance = appearance
+            UITabBar.appearance().scrollEdgeAppearance = appearance
         }
     }
 }
